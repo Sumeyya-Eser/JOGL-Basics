304e13a1adb03e10cb155ae4b9884aed448389c4
Sumeyya-Eser <56974201+Sumeyya-Eser@users.noreply.github.com>
2021-07-17 12:44:47 +0200

    Primitive

---
+++ JOGLPrimitives/src/BasicLine.java
@@ -0,0 +1,49 @@
+import javax.media.opengl.*;  
+import javax.media.opengl.awt.GLCanvas;  
+import javax.swing.JFrame;  
+  
+public class BasicLine implements GLEventListener {  
+  
+    @Override  
+public void init(GLAutoDrawable arg0)   
+  {  
+          
+  }  
+  
+   @Override  
+public void display(GLAutoDrawable drawable) {  
+final GL2 gl = drawable.getGL().getGL2();  
+  
+gl.glBegin (GL2.GL_LINES);  
+gl.glVertex2d(-0.60, 0.10);  
+gl.glVertex2d(0.60, 0.10);  
+gl.glEnd();  
+  
+   }        
+   @Override  
+public void reshape(GLAutoDrawable arg0, int arg1, int arg2, int arg3, int arg4)   
+   {      
+  
+   }  
+   @Override  
+public void dispose(GLAutoDrawable arg0)   
+   {  
+  
+   }  
+  
+public static void main(String[] args) {  
+  
+final GLProfile gp = GLProfile.get(GLProfile.GL2);  
+GLCapabilities cap = new GLCapabilities(gp);  
+  
+final GLCanvas gc = new GLCanvas(cap);  
+BasicLine bl = new BasicLine();  
+gc.addGLEventListener(bl);  
+gc.setSize(400, 400);  
+  
+final JFrame frame = new JFrame ("JOGL Line");  
+frame.add(gc);  
+frame.setSize(500,400);  
+frame.setVisible(true);    
+   }      
+} 
